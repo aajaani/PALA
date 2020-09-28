af45dbf6381e221563c4d6903d639cdf9f086d7d
renekutt <kytt.rene@gmail.com>
2020-09-28 08:12:56 +0300

    Added initial layout

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,18 @@
 $(function() {
     console.log( "ready!" );
+    $(".custom-file-input").on("change", function() {
+        var fileName = $(this).val()//.split("\\").pop();
+        $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
+      });
+
+    addListeners();
 });
+
+function addListeners(){
+    $(".input-group-append").click(fileSubmit);
+}
+
+function fileSubmit(){
+    console.log(this.previousElementSibling.firstElementChild.value);
+    console.log(this.previousElementSibling.firstElementChild.files);
+}
