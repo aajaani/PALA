3a0350cbbbb4c81964b205372a5941451d8a1055
Rene Kütt <kytt.rene@gmail.com>
2020-09-30 22:51:15 +0300

    Expanded single file analysation

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 $(function() {
     console.log( "ready!" );
     $(".custom-file-input").on("change", function() {
-        var fileName = $(this).val()//.split("\\").pop();
+        var fileName = $(this).val().split("\\").pop();
         $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
       });
 
@@ -37,7 +37,8 @@ function analyse(jsonLog){
     const startTime=new Date(jsonLog[0].time);
     const endTime=new Date(jsonLog[jsonLog.length-1].time);
     const elapsedDate=new Date(endTime-startTime);
-    var elapsedTime=elapsedDate.toISOString().slice(11, -1);
+    var elapsedTime=elapsedDate.toISOString().slice(11, -5).split(":");
+    elapsedTime=elapsedTime[0].concat("h, ",elapsedTime[1],"min, ",elapsedTime[2],"sec");
     if(elapsedDate.getDate()>1){
         elapsedTime=(elapsedDate.getDate()-1).toString().concat(" days, ", elapsedTime)
     }
@@ -45,36 +46,70 @@ function analyse(jsonLog){
     var errorCount=0;
     var runCount=0;
     var copyPasteCount=0;
+    var debugCount=0;
+    var filesCreated="";
+    var copiedTexts={};
+    var errorTexts={};
     for(var i=0;i<jsonLog.length;i++){
         if(jsonLog[i].sequence==='ShellCommand' && jsonLog[i].command_text.slice(0,4)==='%Run'){
             runCount++;
         }
         if(jsonLog[i].sequence==='TextInsert' && jsonLog[i].text.includes('Error')){
             errorCount++;
+            var date=getDate1(jsonLog[i].time)
+            errorTexts[date]=jsonLog[i].text;
+        }
+        if(jsonLog[i].sequence==='TextInsert' && jsonLog[i].text.includes('Debug')){
+            debugCount++;
         }
         if(jsonLog[i].sequence==='<<Paste>>' && jsonLog[i].text_widget_class==="CodeViewText"){
             copyPasteCount++;
+            var date=getDate1(jsonLog[i-1].time)
+            copiedTexts[date]=jsonLog[i-1].text;
+        }
+        if(jsonLog[i].sequence==='SaveAs'){
+            var filename=jsonLog[i].filename.split('\\');
+            filesCreated+=filename[filename.length-1].concat('<br>');
         }
     }
     
     var generalInfo={
-        'startTime':startTime,
-        'endTime':endTime,
-        'elapsedTime':elapsedTime,
-        'errorCount':errorCount,
-        'runCount':runCount,
-        'copyPasteCount':copyPasteCount
+        'Start time':startTime.toLocaleString('en-GB'),
+        'End time':endTime.toLocaleString('en-GB'),
+        'Elapsed time':elapsedTime,
+        'Run cunt':runCount,
+        'Error count':errorCount,
+        'Copy-paste count':copyPasteCount,
+        'Debug count':debugCount,
+        'Files created':filesCreated
     }
+
     if($('#general-info-table-c').hasClass('d-none')){
         $('#general-info-table-c').removeClass('d-none');
     }
-    displayDataTable('general-info-table-c',generalInfo);
+    if($('#copiedTexts').hasClass('d-none')){
+        $('#copiedTexts').removeClass('d-none');
+    }
+    if($('#errorTexts').hasClass('d-none')){
+        $('#errorTexts').removeClass('d-none');
+    }
+    displayDataTable('general-info-table',generalInfo);
+    displayDataTable('tableCopyPaste',copiedTexts);
+    displayDataTable('tableErrors',errorTexts);
 }
 
-function displayDataTable(tableContId,data){
-    const tbody=$('#'+tableContId+" table tbody");
+function displayDataTable(tableId,data){
+    const tbody=$('#'+tableId+" tbody");
     tbody.empty();
     for(const key in data){
+        if(Array.isArray(data[key])){
+            console.log('jap');
+            continue;
+        }
         tbody.append('<tr><td>'.concat(key,'</td><td>',data[key],'</td></tr>'));
     }
+}
+
+function getDate1(date){
+    return new Date(date).toLocaleString('en-GB');
 }
